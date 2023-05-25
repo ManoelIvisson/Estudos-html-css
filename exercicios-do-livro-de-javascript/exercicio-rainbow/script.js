cb8bc10dd0c0cefc93ea4fe70c4998f5f324450c
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-05-25 11:03:21 -0300

    fazendo um arco iris

---
+++ exercicios-do-livro-de-javascript/exercicio-rainbow/script.js
@@ -0,0 +1,14 @@
+const botao = window.document.getElementById('botao')
+var num = window.document.querySelector('p')
+
+const cores = ['red', 'green', 'blue', 'gray', 'pink', 'yellow', 'orange', 'darkblue']
+
+botao.addEventListener('click', mudaCor)
+
+function mudaCor() {
+    cor_aleatoria = Math.floor(8*Math.random())
+
+    num.innerHTML = 'numero: ' + cor_aleatoria
+    
+    window.document.body.style.backgroundColor = cores[cor_aleatoria]
+}
