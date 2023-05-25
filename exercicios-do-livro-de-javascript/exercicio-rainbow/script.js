a855f2b8b2f2472171ff9dfc0a239a6b2e541a75
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-05-25 11:10:40 -0300

    Update script.js

--- exercicios-do-livro-de-javascript/exercicio-rainbow/script.js
+++ exercicios-do-livro-de-javascript/exercicio-rainbow/script.js
@@ -1,12 +1,12 @@
 const botao = window.document.getElementById('botao')
 var num = window.document.querySelector('p')
 
-const cores = ['red', 'green', 'blue', 'gray', 'pink', 'yellow', 'orange', 'darkblue']
+const cores = ['red', 'green', 'blue', 'gray', 'pink', 'yellow', 'orange', 'darkblue', 'rebeccapurple']
 
 botao.addEventListener('click', mudaCor)
 
 function mudaCor() {
-    cor_aleatoria = Math.floor(8*Math.random())
+    cor_aleatoria = Math.floor(9*Math.random())
 
     num.innerHTML = 'numero: ' + cor_aleatoria
     
